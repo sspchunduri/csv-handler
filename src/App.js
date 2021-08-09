8c334d5788737121d715b04a878ac2b0135dba63
ssp chunduri <thewhitecollarguy@protonmail.com>
2021-08-09 17:02:19 +0530

    csv-handler

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react'
+import './App.css'
+import MaterialTable from 'material-table'
+import XLSX from 'xlsx'
+const EXT = ['xlsx','xls', 'csv']
 
-function App() {
+
+function App(){
+  const [colHeaders, setColHeaders] = useState()
+  const [data, setData] = useState()
+
+  const getExt = (file) => {
+    const before = file.name.split('.')
+    const extn = before[before.length - 1]
+    return EXT.includes(extn)
+  }
+
+  const convToJson = (headers, data) => {
+    const rows = []
+    data.forEach(row => {
+      let rowData = {}
+      row.forEach((element,index) => {
+        rowData[headers[index]] = element
+      })
+      rows.push(rowData)
+    })
+    return rows
+  }
+
+  const imp = (e) => {
+    const file = e.target.files[0]
+    const reader = new FileReader()
+    reader.onload = (event) => {
+      const st = event.target.result
+      const book = XLSX.read(st,{type:"binary"})
+      const sheetName = book.SheetNames[0]
+      const sheet = book.Sheets[sheetName]
+      const fileData = XLSX.utils.sheet_to_json(sheet, {header:1})
+      const headers = fileData[0]
+      const heads = headers.map(head=> ({
+        title: head,
+        field: head
+      }))
+      setColHeaders(heads)
+      fileData.splice(0,1)
+      setData(convToJson(headers,fileData))
+    }
+    if (file) {
+      if (getExt(file)) {
+        reader.readAsBinaryString(file)
+      }
+      else {
+        alert("Invalid File extension. CSV or Excel files only!")
+      }
+    }
+    else {
+      setData([])
+      setColHeaders([])
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 align="center">CSV-Handler</h1>
+      <h3 align="center">Import your CSV, XLSX, or XLS file</h3>
+      <input type="file" onChange={imp} />
+      <MaterialTable title="Your Data" data={data} columns={colHeaders} />
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
